a06393a47b95fbe10442aa2b1d4e191cceecadba
Jacob Templeton <jacob.templeton@gmail.com>
2023-11-03 15:10:37 -0700

    Add client query that returns the postgres notes data, add basic rendering of that data to react frontend

--- frontend/graphql-notes/src/App.jsx
+++ frontend/graphql-notes/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_NOTES = gql`
+  query {
+    allNotes {
+      note_id
+      user_id
+      username
+      content
+      is_important
+      is_public
+      created_at
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const result = useQuery(ALL_NOTES);
 
+  console.log("DATA", result.data);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{result.data.allNotes.map((p) => p.content).join(", ")}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
