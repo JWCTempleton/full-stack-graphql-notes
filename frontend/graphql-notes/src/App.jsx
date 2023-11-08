93b73477f1760f72445d91815c4a6841f48b7010
Jacob Templeton <jacob.templeton@gmail.com>
2023-11-07 16:56:07 -0800

    Add LoginForm to handle allowing users to log in, conditionally render a logout button and allow for logout if a user is logged in

--- frontend/graphql-notes/src/App.jsx
+++ frontend/graphql-notes/src/App.jsx
@@ -1,19 +1,37 @@
 import "./App.css";
-import { useQuery } from "@apollo/client";
+import { useApolloClient, useQuery } from "@apollo/client";
 import Notes from "./components/Notes";
 import NoteForm from "./components/NoteForm";
+import LoginForm from "./components/LoginForm";
 import { ALL_NOTES } from "./queries";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState(null);
   const result = useQuery(ALL_NOTES);
-
-  console.log("DATA", result.data);
+  const client = useApolloClient();
 
   if (result.loading) {
     return <div>loading...</div>;
   }
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <div>
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} />
+      </div>
+    );
+  }
   return (
     <>
+      <button onClick={logout}>logout</button>
       <NoteForm />
       <h1>Notes</h1>
       <Notes notes={result.data.allNotes} />
